e67ddf4a37fc61a6d00ebbc7194666100a807385
Ksenia Coulter <kseniacoulter@gmail.com>
2016-12-10 15:16:17 -0500

    load sample fishes works

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -38,7 +38,11 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
-            <Fish />
+            {
+              Object
+              .keys(this.state.fishes) //Object.keys returns an array that can be itirated over
+              .map(key => <Fish  key={key} details={this.state.fishes[key]} />)
+            }
           </ul>
         </div>
           <Order />
